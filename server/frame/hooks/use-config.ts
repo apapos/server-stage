e71b1d1815feb03efea12ad598d750228ba04ee0
ll526e <ll526e@163.com>
2024-05-09 15:52:13 +0800

    chore: upgrade

--- server/frame/hooks/use-config.ts
+++ server/frame/hooks/use-config.ts
@@ -1,6 +1,6 @@
 export const useConfig = (extraParams?) => {
     return {
-        base_url: 'https://relay-amber.vercel.app',
+        base_url: 'https://tmdb.ll526e.cn',
         params: {
             // 'language': 'zh-CN',
             ...extraParams
